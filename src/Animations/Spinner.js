b60778fee40efc7ac7570896790434fbf35f70bd
jameelSalhhadin <69424795+LoxductheGreat@users.noreply.github.com>
2022-12-14 14:55:23 +0800

    displays all users in chat with images

--- src/Animations/Spinner.js
+++ src/Animations/Spinner.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Spinner () {
   return (
-    <div class='spinner-border text-secondary m-auto align-self-center w-25 h-50' role='status'>
-      <span class='visually-hidden'>Loading...</span>
+    <div className='spinner-border text-secondary m-auto align-self-center w-25 h-50' role='status'>
+      <span className='visually-hidden'>Loading...</span>
     </div>
   )
 }
--- src/Components/ScrollableChat.js
+++ src/Components/ScrollableChat.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import ScrollableFeed from 'react-scrollable-feed'
 import { ChatState } from '../Context/ChatContext'
-import { isLastMessage, isSameSender } from './ChatLogic'
+import { isLastMessage, isSameSender, isSameSenderMargin, isSameUser } from './ChatLogic'
 
 function ScrollableChat ({ messages }) {
   const { user } = ChatState()
   return (
     <ScrollableFeed>
-      {messages && messages.map((m, i) => (
-        <div className='d-flex' key={m._id}>
-          {(isSameSender(messages, m, i, user._id) ||
-            isLastMessage(messages, i, user._id)) && (
-            <div className=''>
-              <img className='chatimg' data-bs-toggle='tooltip' data-bs-placement='bottom' data-bs-title='Tooltip on bottom' src={m.sender.pic} />
-            </div>
-          )}
-        </div>
-      ))}
+      {messages &&
+        messages.map((m, i) => (
+          <div className='d-flex nice' key={m._id}>
+            {(isSameSender(messages, m, i, user._id) ||
+              isLastMessage(messages, i, user._id)) && (
+              <img className='chatimg rounded-circle' name={m.sender.username} src={m.sender.pic} data-bs-toggle='tooltip' data-bs-placement='bottom' data-bs-title='Tooltip on top' />
+            )}
+
+            <span style={{ backgroundColor: `${m.sender._id === user._id ? '#BEE3F8' : '#B9F5D0'}`, borderRadius: '20px', padding: '5px 15px', maxWidth: '75%', marginLeft: isSameSenderMargin(messages, m, i, user._id), marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10 }}>
+              {m.content}
+            </span>
+          </div>
+        ))}
     </ScrollableFeed>
   )
 }
