07c9ee26ce55094088b9fabf613ddd7dabffa416
jameelSalhhadin <69424795+LoxductheGreat@users.noreply.github.com>
2022-12-16 17:17:17 +0800

    notication changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,6 +8,8 @@ import axios from 'axios'
 import ChatItemList from './ChatItemList'
 import Spinner from '../Animations/Spinner'
 import { getSender } from './ChatLogic'
+import NotificationBadge from 'react-notification-badge'
+import { Effect } from 'react-notification-badge'
 
 function Header () {
   const [search, setSearch] = useState('')
@@ -88,7 +90,7 @@ function Header () {
       <h4 className='m-1 my-sm-0'>Chatify</h4>
       <div className='btn-group-sm mx-4' role='group' aria-label='Button group with nested dropdown'>
         {/* Might wanna use a badge for notiication bell */}
-        <button type='button' className='btn btn-light notti-button dropdown-toggle' data-bs-toggle='dropdown'><FontAwesomeIcon size='xl' icon={faBell} />
+        <button type='button' className='btn btn-light notti-button dropdown-toggle' data-bs-toggle='dropdown'><NotificationBadge count={notification.length} effect={Effect.SCALE} /><FontAwesomeIcon size='xl' icon={faBell} />
         </button>
 
         {/* DropDown */}
@@ -132,56 +134,6 @@ function Header () {
         </div>
       </div>
     </nav>
-    // <nav className='navbar navbar-light bg-light'>
-    //   <button className='d-line btn btn-light ' type='button' data-bs-toggle='offcanvas' data-bs-target='#staticBackdrop' aria-controls='staticBackdrop'>
-    //     <FontAwesomeIcon icon={faMagnifyingGlass} className='align-baseline my-2 my-sm-0 px-1' size='sm' /> Search User
-    //   </button>
-    //   <div className='offcanvas offcanvas-start w-auto' data-bs-backdrop='static' tabIndex='-1' id='staticBackdrop' aria-labelledby='staticBackdropLabel'>
-    //     <div className='offcanvas-header'>
-    //       <h4 className='offcanvas-title x' id='staticBackdropLabel'>Search Users</h4>
-    //       <button type='button' className='btn-close' data-bs-dismiss='offcanvas' aria-label='Close' />
-    //     </div>
-    //     <div className='offcanvas-body'>
-    //       <div className='input-group mb-3'>
-    //         <input type='text' className='form-control' placeholder='Search by name or email' aria-describedby='button-addon1' value={search} onChange={e => setSearch(e.target.value)} />
-    //         <button className='btn btn-primary' type='button' id='button-addon1' onClick={onSearch}>Search</button>
-    //       </div>
-    //       {loading ? (<ChatLoading />) : (
-    //         searchResult?.map(user => (
-    //           <ChatItemList key={user._id} user={user} handleFunction={() => accessChat(user._id)} />
-    //         ))
-    //       )}
-    //     </div>
-    //   </div>
-    //   <h3 className='navbar-brand m-1 my-sm-0 title'>Chatify</h3>
-    //   <div className='btn-group-sm mx-4' role='group' aria-label='Button group with nested dropdown'>
-    //     {/* Might wanna use a badge for notiication bell */}
-    //     <button type='button' className='btn btn-light notti-button' data-bs-container='body' data-bs-toggle='popover' data-bs-placement='bottom' data-bs-content='Bottom popover'><FontAwesomeIcon size='xl' icon={faBell} />
-    //     </button>
-    //     <button type='button' className='btn btn-light '><img className='rounded header-img' src={user.pic} alt='' /></button>
-    //     <div className='btn-group' role='group'>
-    //       <button type='button' className='btn btn-primary dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false' />
-    //       <ul className='dropdown-menu dropdown-menu-end'>
-    //         <li><button type='button' className='dropdown-item' data-bs-toggle='modal' data-bs-target='#staticModel'>My Profile</button></li>
-    //         <li><button className='dropdown-item' onClick={onLogout}>Logout</button></li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    //   <div className='modal fade' id='staticModel' data-bs-backdrop='static' data-bs-keyboard='false' tabIndex='-1' aria-labelledby='staticModelLabel' aria-hidden='true'>
-    //     <div className='modal-dialog'>
-    //       <div className='modal-content'>
-    //         <div className='modal-body text-center'>
-    //           <h1>{user.username}</h1>
-    //           <img className='profile-img rounded p-2' src={user.pic} alt='' />
-    //           <h2 className='text-secondary'>{user.email}</h2>
-    //         </div>
-    //         <div className='modal-footer'>
-    //           <button type='button' className='btn btn-primary' data-bs-dismiss='modal'>Close</button>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </nav>
   )
 }
 
